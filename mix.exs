a206d18ad4ac7e11234e6b6d228a43cf5e231eb0
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2022-08-31 13:08:36 +0200

    Updated dependencies

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Keyring.MixProject do
   use Mix.Project
 
   def project do
-    [app: :keyring,
+    [
+      app: :keyring,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: [main_module: Keyring, name: "keyring"]]
+      escript: [main_module: Keyring, name: "keyring"]
+    ]
   end
 
   def application do
@@ -15,7 +17,9 @@ defmodule Keyring.MixProject do
   end
 
   defp deps do
-    [{:plug_crypto, "~> 1.0"},
-      {:ok, "~> 2.3"}]
+    [
+      {:plug_crypto, "~> 1.0"},
+      {:ok, "~> 2.3"},
+    ]
   end
 end
